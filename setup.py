587abb3b9486c6a16a5fac999ebd5ff3cec1cfda
Dave Hughes <dave@waveform.org.uk>
2013-02-23 20:15:44 +0000

    Updated setup script with py2exe stuff
    Included workaround for a unicode bug in py2exe, and some stuff to
    convert setuptools entry points to py2exe entry points (which are rather
    more basic)...

--- setup.py
+++ setup.py
@@ -25,10 +25,25 @@ from __future__ import (
     )
 
 import os
+import sys
 from setuptools import setup, find_packages
 from utils import description, get_version, require_python
 
 HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Workaround a silly bug in py2exe's unicode handling which results in
+# "TypeError: decoding Unicode is not supported" when passing unicode
+# strings to setup(). Patch submitted to SF site: #3605752
+if py2exe:
+    import py2exe.resources.StringTables
+    def w32_uc(text):
+        """convert a string into unicode, then encode it into UTF-16
+        little endian, ready to use for win32 apis"""
+        if isinstance(text, unicode):
+            return text.encode("utf-16-le")
+        else:
+            return unicode(text, "unicode-escape").encode("utf-16-le")
+    py2exe.resources.StringTables.w32_uc = w32_uc
 
 # Workaround <http://bugs.python.org/issue10945>
 import codecs
@@ -82,6 +97,49 @@ ENTRY_POINTS = {
     ]
     }
 
+OPTIONS = {}
+
+if py2exe:
+    # Construct Windows-specific build options
+    OPTIONS['py2exe'] = {
+        'compressed': True,
+        # No idea why py2exe wants to include all these "missing" modules,
+        # but we don't need them so just exclude the lot
+        'excludes': [
+            'IronPythonConsole',
+            'System',
+            'System.Windows.Forms.Clipboard',
+            '_scproxy',
+            'clr',
+            'modes.editingmodes',
+            'startup',
+            ],
+        # We fill out the console and GUI entry points below. No idea why
+        # py2exe can't just use the setuptools entry points...
+        'console': [],
+        'windows': [],
+        }
+    for entry_point_group, target_key in [
+            ('console_scripts', 'console'),
+            ('gui_scripts', 'windows'),
+            ]:
+        for entry_point in ENTRY_POINTS[entry_point_group]:
+            # Extract module/package name from the entry point
+            entry_point = entry_point.split('=', 1)[1].strip().split(':', 0)[0]
+            # Convert the module/package name to a file path
+            entry_point = os.path.join(
+                os.path.dirname(__file__),
+                entry_point.replace('.', os.path.sep))
+            if os.path.isdir(entry_point):
+                entry_point = os.path.join(entry_point, '__init__.py')
+            else:
+                entry_point += '.py'
+            OPTIONS['py2exe'][target_key].append(entry_point)
+
+if py2app:
+    # Mac specific build options
+    OPTIONS['py2app'] = {}
+
 
 def main():
     setup(
@@ -102,7 +160,8 @@ def main():
         zip_safe             = False,
         test_suite           = 'rastools',
         entry_points         = ENTRY_POINTS,
-    )
+        options              = OPTIONS,
+        )
 
 if __name__ == '__main__':
     main()
